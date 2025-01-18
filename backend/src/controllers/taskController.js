c01e6364c0acac5200e164655e4bc600743683c5
Ej Sobrepeña <sobrepenaej@yahoo.com>
2025-01-18 04:20:58 +0200

    Update dependencies and improve task API functionality

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,55 +1,56 @@
-const taskModel = require('../models/taskModel');
+const taskModel = require("../models/taskModel");
 
 module.exports.getTasks = async (req, res) => {
-    const tasks = await taskModel.find()
-    res.send(tasks)
-}
+  const tasks = await taskModel.find();
+  res.send(tasks);
+};
 
 module.exports.saveTask = async (req, res) => {
-    const task = new taskModel({
-        list: req.body
-    });
+  const task = new taskModel({
+    list: req.body,
+  });
 
-    task.save()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.json({ message: err });
-        });
-}
+  task
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+};
 
 module.exports.getTask = async (req, res) => {
-    try {
-        const task = await taskModel.findById(req.params.id);
-        res.json(task);
-    } catch (err) {
-        res.json({ message: err });
-    }
-}
+  try {
+    const task = await taskModel.findById(req.params.id);
+    res.json(task);
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
 
 module.exports.updateTask = async (req, res) => {
+  const task = {
+    list: req.body,
+  };
 
-    const task = {
-        list: req.body
-    };
-
-    console.log(task);
-    taskModel.findByIdAndUpdate({ _id: req.params.id }, task)
-        .then(() => {
-            taskModel.findOne({ _id: req.params.id })
-                .then((task) => res.json(task))
-        })
-}
+  console.log(task);
+  taskModel.findByIdAndUpdate({ _id: req.params.id }, task).then(() => {
+    taskModel.findOne({ _id: req.params.id }).then((task) => res.json(task));
+  });
+};
 
 module.exports.deleteTask = async (req, res) => {
-    try {
-        const removeTask = await taskModel.remove({ _id: req.params.id });
-        res.json(removeTask);
-    } catch (err) {
-        res.json({ message: err });
+  try {
+    const removedTask = await taskModel.findByIdAndDelete(req.params.id);
+    if (!removedTask) {
+      return res.status(404).json({ message: "Task not found" });
     }
-}
+    res.json({ message: "Task deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 // original update
 // module.exports.updateTask = async (req, res) => {
